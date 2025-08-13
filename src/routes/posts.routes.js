a98d14810e8112093ffa77651b1235d328f88203
Levy <85988919231levy@gmail.com>
2025-08-13 09:54:13 -0300

    feat: Criando estrutura do nest

--- src/routes/posts.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-import comunidadesController from '../controllers/comunidades.controller.js'
-
-const postsRouter = express.Router()
-
-postsRouter.get('/', comunidadesController.index)
-
-export default postsRouter
